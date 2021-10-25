04e422f365767030ab653f9609896a360f5b7f3b
palmerpesta1 <pjp5387@psu.edu>
2021-10-24 21:37:12 -0400

    trying to fix

--- YOURCODEHERE.cpp
+++ YOURCODEHERE.cpp
@@ -236,6 +236,8 @@ int validateConfiguration(std::string configuration) {
 	bool check4 = 2048 <= DL1CacheSize <= 65536;
 
 	bool check5 = 32768 <= UL2CacheSize <= 1048576;
+
+	cout << IL1CacheSize;
 	
 	return(isNumDimConfiguration(configuration) && check1 && check2 && check3 && check4 && check5);
 }
